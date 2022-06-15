66115b96bec3a409e4205b69f66fc62a98a256e5
aashay <aashay@farintsol.com>
2022-06-15 17:48:37 +0530

    image and misc changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,171 +1,173 @@
-import {Component} from '@angular/core';
-import {HostListener} from '@angular/core';
-import {Observable} from 'rxjs';
-import {map, share} from 'rxjs/operators';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { Component } from "@angular/core";
+import { HostListener } from "@angular/core";
+import { Observable } from "rxjs";
+import { map, share } from "rxjs/operators";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
-import {TranslateService} from '@ngx-translate/core';
-import {UserService} from './core/services/user.service';
+import { TranslateService } from "@ngx-translate/core";
+import { UserService } from "./core/services/user.service";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-    constructor(private breakpointObserver: BreakpointObserver, public translate: TranslateService, public userService: UserService) {
-        this.innerWidth = window.innerWidth;
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public translate: TranslateService,
+    public userService: UserService,
+  ) {
+    this.innerWidth = window.innerWidth;
 
-        translate.addLangs(['en', 'de']);
-        translate.setDefaultLang('de');
+    translate.addLangs(["en", "de"]);
+    translate.setDefaultLang("de");
 
-        const browserLang = translate.getBrowserLang();
-        this.userService.currentLanguage.next('de');
-    }
+    const browserLang = translate.getBrowserLang();
+    this.userService.currentLanguage.next("de");
+  }
 
-    toggleAb: boolean;
+  toggleAb: boolean;
 
-    title = 'Physiotherapie Glattbrugg';
+  title = "Physiotherapie Glattbrugg";
 
-    innerWidth: any;
+  innerWidth: any;
 
-    showMenu: boolean;
+  showMenu: boolean;
 
-    showSubMenu: boolean;
+  showSubMenu: boolean;
 
-    showSubMenu1: boolean;
+  showSubMenu1: boolean;
 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.Tablet, Breakpoints.Handset])
+    .pipe(
+      map((result) => result.matches),
+      share(),
+    );
 
-    isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Tablet, Breakpoints.Handset])
-        .pipe(
-            map(result => result.matches),
-            share()
-        );
+  massages = [
+    {
+      path: "manual-therapy",
+      name: "Manuelle Therapie",
+    },
+    {
+      path: "electrotherapy",
+      name: "Elektrotherapie",
+    },
+    {
+      path: "shockwave-therapy",
+      name: "Stosswellentherapie",
+    },
+    {
+      path: "medical-training-therapy",
+      name: "Medizinische Trainingstherapie",
+    },
+    {
+      path: "sports-physiotherapy",
+      name: "Sportphysiotherapie",
+    },
+    {
+      path: "kinesio-taping",
+      name: "Kinesio taping",
+    },
+    {
+      path: "trigger-point-therapy",
+      name: "Triggerpunkttherapie",
+    },
+    {
+      path: "lymphatic-drainage",
+      name: "Lymphdrainage",
+    },
+    {
+      path: "neurologische-therapie",
+      name: "Neurologische Therapie",
+    },
+    {
+      path: "PNF",
+      name: "PNF - Propriozeptive neuromuskuläre Fazilitation",
+    },
+    {
+      path: "cognitive-funktional-therapie",
+      name: "Cognitive Funktional Therapie",
+    },
+    {
+      path: "in-house-treatment",
+      name: "Domizilbehandlung",
+    },
+  ];
 
-    massages = [
-        {
-            path: 'manual-therapy',
-            name: 'Manuelle Therapie'
-        },
-        {
-            path: 'electrotherapy',
-            name: 'Elektrotherapie'
-        },
-        {
-            path: 'shockwave-therapy',
-            name: 'Stosswellentherapie'
-        },
-        {
-            path: 'medical-training-therapy',
-            name: 'Medizinische Trainingstherapie'
-        },
-        {
-            path: 'sports-physiotherapy',
-            name: 'Sportphysiotherapie'
-        },
-        {
-            path: 'kinesio-taping',
-            name: 'Kinesio taping'
-        },
-        {
-            path: 'trigger-point-therapy',
-            name: 'Triggerpunkttherapie'
-        },
-        {
-            path: 'lymphatic-drainage',
-            name: 'Lymphdrainage'
-        },
-        {
-            path: 'neurologische-therapie',
-            name: 'Neurologische Therapie'
-        },
-        {
-            path: 'PNF',
-            name: 'PNF - Propriozeptive neuromuskuläre Fazilitation'
-        },
-        {
-            path: 'cognitive-funktional-therapie',
-            name: 'Cognitive Funktional Therapie'
-        },
-        {
-            path: 'in-house-treatment',
-            name: 'Domizilbehandlung'
-        }
-    ];
+  prevention = [
+    {
+      path: "physiotherapeutic-consultation",
+      name: "Physiotherapy advice",
+    },
+    {
+      path: "massage-therapy",
+      name: "Massage",
+    },
+  ];
+  about = [
+    {
+      path: "phylosophy",
+      name: "Phylosophy",
+    },
+    {
+      path: "team",
+      name: "Team",
+    },
+    {
+      path: "practice",
+      name: "Practice",
+    },
+  ];
 
-    prevention = [
-        {
-            path: 'physiotherapeutic-consultation',
-            name: 'Physiotherapy advice'
-        },
-        {
-            path: 'massage-therapy',
-            name: 'Massage'
-        }
-    ];
-    about = [
-        {
-            path: 'phylosophy',
-            name: 'Phylosophy'
-        },
-        {
-            path: 'about-me',
-            name: 'About me'
-        },
-        {
-            path: 'practice',
-            name: 'Practice'
-        }
-    ];
+  lng: any[] = [
+    {
+      value: "de",
+      name: "Deutsch",
+      img: "../../../../assets/img/switzerland.png",
+    },
+    {
+      value: "en",
+      name: "English",
+      img: "../../../../assets/img/english.png",
+    },
+  ];
 
-    lng: any[] = [
-        {
-            value: 'de',
-            name: 'Deutsch',
-            img: '../../../../assets/img/switzerland.png'
-        },
-        {
-            value: 'en',
-            name: 'English',
-            img: '../../../../assets/img/english.png'
-        }
-    ];
+  current: any = this.lng[0];
 
-    current: any = this.lng[0];
+  langOpen: boolean;
 
-    langOpen: boolean;
+  setLanguage(lang) {
+    this.translate.use(lang.value);
+    this.current = lang;
+    this.userService.currentLanguage.next(lang.value);
+  }
 
-    setLanguage(lang) {
-        this.translate.use(lang.value);
-        this.current = lang;
-        this.userService.currentLanguage.next(lang.value);
-    }
+  toggleLang() {
+    this.langOpen = !this.langOpen;
+  }
 
-    toggleLang() {
-        this.langOpen = !this.langOpen;
-    }
+  toggleAbout() {
+    this.toggleAb = !this.toggleAb;
+  }
 
-    toggleAbout() {
-        this.toggleAb = !this.toggleAb;
-    }
+  togle() {
+    this.showMenu = !this.showMenu;
+  }
 
-    togle() {
-        this.showMenu = !this.showMenu;
-    }
+  togleSubMenu() {
+    this.showSubMenu = !this.showSubMenu;
+  }
 
-    togleSubMenu() {
-        this.showSubMenu = !this.showSubMenu;
-    }
+  togleSubMenu1() {
+    this.showSubMenu1 = !this.showSubMenu1;
+  }
 
-    togleSubMenu1() {
-        this.showSubMenu1 = !this.showSubMenu1;
-    }
-
-    @HostListener('window:resize')
-    public onResize(): void {
-        this.innerWidth = window.innerWidth;
-        console.log(this.innerWidth);
-    }
-
-
+  @HostListener("window:resize")
+  public onResize(): void {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+  }
 }
